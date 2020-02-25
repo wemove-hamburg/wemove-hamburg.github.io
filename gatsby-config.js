a761f86bd29f8efd048b9f24697d3923f22e005e
Peter Pult <peter@pult.cc>
2020-02-25 22:30:53 +0100

    feat: shadow and replace all necessary files to start

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,66 +1,62 @@
-require(`dotenv`).config({
-  path: `.env`
+require('dotenv').config({
+  path: '.env'
 });
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`
+    siteTitle: 'WeMove Hamburg',
+    siteTitleAlt: 'WeMove Hamburg - Mobilitätsbericht 2020',
+    siteUrl: 'https://wemove.hamburg',
+    siteLanguage: 'de',
+    siteImage: '/banner.jpg',
+    author: '@ppult'
   },
   plugins: [
     {
-      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      resolve: '@lekoarts/gatsby-theme-minimal-blog',
       options: {
         navigation: [
           {
-            title: `Blog`,
-            slug: `/blog`
-          },
-          {
-            title: `About`,
-            slug: `/about`
+            title: 'Über uns',
+            slug: '/ueber-uns'
           }
         ],
-        externalLinks: [
-          {
-            name: `Twitter`,
-            url: `https://twitter.com/ppult`
-          }
-        ]
+        externalLinks: []
       }
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_ID
       }
     },
-    `gatsby-plugin-sitemap`,
+    'gatsby-plugin-sitemap',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#6B46C1`,
-        display: `standalone`,
+        name: 'WeMove Hamburg - Mobilitätsbericht 2020 ',
+        short_name: 'WeMove Hamburg',
+        description:
+          'Der Bericht zur Lage der Mobilität in Hamburg, hat eine objektive Darstellung zum Ziel. Jeder ist eingeladen zu diesem Bericht beizutragen und seine Sicht hinzuzufügen. Er dient der Grundlage und Gegenüberstellung, nicht der Meinungsmache.',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#6B46C1',
+        display: 'standalone',
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
           {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
           }
         ]
       }
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`
-    // `gatsby-plugin-webpack-bundle-analyser-v2`,
+    'gatsby-plugin-offline',
+    'gatsby-plugin-netlify'
   ]
 };
