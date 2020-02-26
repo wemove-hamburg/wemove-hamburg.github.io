a7a3408a6b2caaaec988e27f73661cf356698034
Peter Pult <peter@pult.cc>
2020-02-26 23:57:29 +0100

    feat: add audience draft to home

--- src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.js
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.js
@@ -2,14 +2,19 @@
 import { jsx } from 'theme-ui';
 
 import Layout from '@lekoarts/gatsby-theme-minimal-blog/src/components/layout';
+import Title from '@lekoarts/gatsby-theme-minimal-blog/src/components/title';
 import Hero from '../texts/hero';
+import Audience from '../texts/audience';
 
 const Homepage = () => {
   return (
     <Layout>
-      <section sx={{ mb: [5, 6, 7], p: { fontSize: [1, 2, 3], mt: 2 } }}>
+      <section sx={{ mb: [5, 6, 7], fontSize: [1, 2, 3], mt: 2 }}>
         <Hero />
       </section>
+
+      <Title text="Die Bewegten" />
+      <Audience />
     </Layout>
   );
 };
